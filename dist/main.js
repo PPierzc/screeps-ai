af562fe1b0cf3e813d420a758a6e1c96a791b0b0
Paweł <ppierzc@gmail.com>
2019-08-21 00:15:12 +0100

    [FEATURE] dist

--- dist/main.js
+++ dist/main.js
@@ -1,55 +1,8 @@
-module.exports =
-/******/ (function(modules, runtime) { // webpackBootstrap
-/******/ 	"use strict";
-/******/ 	// The module cache
-/******/ 	var installedModules = {};
-/******/
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId]) {
-/******/ 			return installedModules[moduleId].exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			i: moduleId,
-/******/ 			l: false,
-/******/ 			exports: {}
-/******/ 		};
-/******/
-/******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
-/******/ 		// Flag the module as loaded
-/******/ 		module.l = true;
-/******/
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/
-/******/
-/******/ 	__webpack_require__.ab = __dirname + "/";
-/******/
-/******/ 	// the startup function
-/******/ 	function startup() {
-/******/ 		// Load entry module and return exports
-/******/ 		return __webpack_require__(31);
-/******/ 	};
-/******/
-/******/ 	// run startup
-/******/ 	return startup();
-/******/ })
-/************************************************************************/
-/******/ ({
-
-/***/ 31:
-/***/ (function(module, __unusedexports, __webpack_require__) {
-
-const HarvesterManager = __webpack_require__(483)
-const UpgraderManager = __webpack_require__(542)
-const DemandManager = __webpack_require__(925)
-const SpawnQueue = __webpack_require__(638)
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+const HarvesterManager = require('./managers/harvester')
+const UpgraderManager = require('./managers/upgrader')
+const DemandManager = require('./managers/demand')
+const SpawnQueue = require('./queues/spawn')
 
 module.exports.loop = function () {
   const {
@@ -65,15 +18,105 @@ module.exports.loop = function () {
   UpgraderManager.tick(upgraders)
 }
 
+},{"./managers/demand":2,"./managers/harvester":3,"./managers/upgrader":4,"./queues/spawn":5}],2:[function(require,module,exports){
+/* global Game */
+module.exports = () => {
+  const { creeps } = Game
 
-/***/ }),
+  const currHarvesters = Object.values(creeps).filter(creep => creep.memory.role === 'harvester').length
+  const currUpgraders = Object.values(creeps).filter(creep => creep.memory.role === 'upgrader').length
 
-/***/ 294:
-/***/ (function(module, __unusedexports, __webpack_require__) {
+  const harvesters = (currUpgraders + 1) * 3
+  const upgraders = Math.floor(currHarvesters / 3)
 
+  return {
+    harvesters,
+    upgraders
+  }
+}
+
+},{}],3:[function(require,module,exports){
+/* global Game */
+
+const HarvesterRole = require('../roles/harvester')
+
+module.exports = {
+  tick: function (demand) {
+    const { creeps } = Game
+
+    const harvesters = Object.values(creeps).filter(creep => creep.memory.role === 'harvester')
+
+    if (harvesters.length < demand) {
+      HarvesterRole.spawn(harvesters.length)
+    }
+
+    harvesters.forEach(harvester => {
+      HarvesterRole.tick(harvester)
+    })
+  }
+}
+
+},{"../roles/harvester":6}],4:[function(require,module,exports){
+/* global Game */
+
+const UpgraderRole = require('../roles/upgrader')
+
+module.exports = {
+  tick: function (demand) {
+    const { creeps } = Game
+
+    const upgraders = Object.values(creeps).filter(creep => creep.memory.role === 'upgrader')
+
+    if (upgraders.length < demand && Object.values(creeps).length) {
+      UpgraderRole.spawn(upgraders.length)
+    }
+
+    upgraders.forEach(upgrader => {
+      UpgraderRole.tick(upgrader)
+    })
+  }
+}
+
+},{"../roles/upgrader":7}],5:[function(require,module,exports){
+/* global Game */
+
+Game.spawns.Spawn1.memory.queue = []
+
+module.exports = {
+  add: (job) => {
+    const {
+      queue
+    } = Game.spawns.Spawn1.memory
+
+    const duplicateJobs = queue.filter(_job => _job.key === job.key)
+
+    if (!duplicateJobs.length) {
+      queue.push(job)
+    }
+  },
+  tick: () => {
+    const spawn = Game.spawns.Spawn1
+
+    if (!spawn.spawning) {
+      if (spawn.memory.processing) {
+        spawn.memory.queue.shift()
+        spawn.memory.processing = false
+      }
+
+      const job = spawn.memory.queue.sort((a, b) => a - b)[0]
+
+      if (job) {
+        spawn.createCreep(job.body, job.name, job.memory)
+        spawn.memory.processing = true
+      }
+    }
+  }
+}
+
+},{}],6:[function(require,module,exports){
 /* global Game, FIND_SOURCES, ERR_NOT_IN_RANGE, RESOURCE_ENERGY, MOVE, WORK, CARRY */
 
-const SpawnQueue = __webpack_require__(638)
+const SpawnQueue = require('../queues/spawn')
 
 module.exports = {
   spawn: function (numberOfHarvesters) {
@@ -99,15 +142,10 @@ module.exports = {
   }
 }
 
-
-/***/ }),
-
-/***/ 394:
-/***/ (function(module, __unusedexports, __webpack_require__) {
-
+},{"../queues/spawn":5}],7:[function(require,module,exports){
 /* global Game, ERR_NOT_IN_RANGE, RESOURCE_ENERGY, MOVE, WORK, CARRY */
 
-const SpawnQueue = __webpack_require__(638)
+const SpawnQueue = require('../queues/spawn')
 
 const upgrade = (creep) => {
   const controller = creep.room.controller
@@ -145,122 +183,4 @@ module.exports = {
   }
 }
 
-
-/***/ }),
-
-/***/ 483:
-/***/ (function(module, __unusedexports, __webpack_require__) {
-
-/* global Game */
-
-const HarvesterRole = __webpack_require__(294)
-
-module.exports = {
-  tick: function (demand) {
-    const { creeps } = Game
-
-    const harvesters = Object.values(creeps).filter(creep => creep.memory.role === 'harvester')
-
-    if (harvesters.length < demand) {
-      HarvesterRole.spawn(harvesters.length)
-    }
-
-    harvesters.forEach(harvester => {
-      HarvesterRole.tick(harvester)
-    })
-  }
-}
-
-
-/***/ }),
-
-/***/ 542:
-/***/ (function(module, __unusedexports, __webpack_require__) {
-
-/* global Game */
-
-const UpgraderRole = __webpack_require__(394)
-
-module.exports = {
-  tick: function (demand) {
-    const { creeps } = Game
-
-    const upgraders = Object.values(creeps).filter(creep => creep.memory.role === 'upgrader')
-
-    if (upgraders.length < demand && Object.values(creeps).length) {
-      UpgraderRole.spawn(upgraders.length)
-    }
-
-    upgraders.forEach(upgrader => {
-      UpgraderRole.tick(upgrader)
-    })
-  }
-}
-
-
-/***/ }),
-
-/***/ 638:
-/***/ (function(module) {
-
-/* global Game */
-
-Game.spawns.Spawn1.memory.queue = []
-
-module.exports = {
-  add: (job) => {
-    const {
-      queue
-    } = Game.spawns.Spawn1.memory
-
-    const duplicateJobs = queue.filter(_job => _job.key === job.key)
-
-    if (!duplicateJobs.length) {
-      queue.push(job)
-    }
-  },
-  tick: () => {
-    const spawn = Game.spawns.Spawn1
-
-    if (!spawn.spawning) {
-      if (spawn.memory.processing) {
-        spawn.memory.queue.shift()
-        spawn.memory.processing = false
-      }
-
-      const job = spawn.memory.queue.sort((a, b) => a - b)[0]
-
-      if (job) {
-        spawn.createCreep(job.body, job.name, job.memory)
-        spawn.memory.processing = true
-      }
-    }
-  }
-}
-
-
-/***/ }),
-
-/***/ 925:
-/***/ (function(module) {
-
-/* global Game */
-module.exports = () => {
-  const { creeps } = Game
-
-  const currHarvesters = Object.values(creeps).filter(creep => creep.memory.role === 'harvester').length
-  const currUpgraders = Object.values(creeps).filter(creep => creep.memory.role === 'upgrader').length
-
-  const harvesters = (currUpgraders + 1) * 3
-  const upgraders = Math.floor(currHarvesters / 3)
-
-  return {
-    harvesters,
-    upgraders
-  }
-}
-
-
-/***/ })
-
-/******/ });
+},{"../queues/spawn":5}]},{},[1]);
