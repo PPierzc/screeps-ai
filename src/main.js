603e1bc9bbbbc040920a8fe35712696c7588c665
Paweł <ppierzc@gmail.com>
2019-08-21 12:16:29 +0100

    [FEATURE] sources manager

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 const CreepsManager = require('./managers/creeps')
+const SourcesManager = require('./managers/sources')
 
 module.exports.loop = function () {
   CreepsManager()
+  SourcesManager()
 }
