5e90b50744ecd1fd18ede8b6c5317386f77e3e0d
Paweł <ppierzc@gmail.com>
2019-08-21 17:04:09 +0100

    [FEATURE] build container

--- src/managers/build.js
+++ src/managers/build.js
@@ -2,7 +2,5 @@ module.exports = () => {
   const { rooms } = Game
   const room = Object.values(rooms)[0]
 
-  const constructRes = room.createConstructionSite(25, 30, STRUCTURE_CONTAINER)
-
-  console.log(constructRes)
+  room.createConstructionSite(25, 30, STRUCTURE_CONTAINER)
 }
