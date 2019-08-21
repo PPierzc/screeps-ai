091e6360d5855eca970987008e39ddd2746aa7a0
Paweł <ppierzc@gmail.com>
2019-08-21 23:58:50 +0100

    [FEATURE] fix structures

--- src/managers/build.js
+++ src/managers/build.js
@@ -2,13 +2,13 @@ module.exports = () => {
   const {
     rooms,
     constructionSites,
-    structures,
     spawns
   } = Game
   const room = Object.values(rooms)[0]
   const spawn = Object.values(spawns)[0]
 
   const containersUnderConstruction = Object.values(constructionSites).filter(constructionSite => constructionSite.structureType === STRUCTURE_CONTAINER)
+  const structures = room.find(FIND_STRUCTURES)
   const containers = Object.values(structures).filter(structure => structure.structureType === STRUCTURE_CONTAINER)
 
   console.log(containers)
