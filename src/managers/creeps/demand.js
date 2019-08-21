0c08f16f5e5c3de5e6dc60d8f583563bfd19fbaf
Paweł <ppierzc@gmail.com>
2019-08-21 23:28:54 +0100

    [FEATURE] demand update

--- src/managers/creeps/demand.js
+++ src/managers/creeps/demand.js
@@ -4,7 +4,7 @@ module.exports = () => {
   const currWorkers = Object.values(creeps).filter(creep => creep.memory.role === 'worker').length
   const currMiners = Object.values(creeps).filter(creep => creep.memory.role === 'miner').length
 
-  const workers = 1
+  const workers = Object.values(creeps).length < 3 ? 1 : Object.values(creeps) / 3
   const upgraders = Math.floor((currMiners) / 3)
   const miners = currWorkers < 1 ? 0 : 3
   const haulers = currMiners
