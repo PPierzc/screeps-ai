adbd9b001f6723f2a2d9eb604983a0e5d48b70c8
Paweł <ppierzc@gmail.com>
2019-08-21 11:40:02 +0100

    [FEATURE] upgrader refactor

--- src/roles/upgrader.js
+++ src/roles/upgrader.js
@@ -1,6 +1,6 @@
 const SpawnQueue = require('../queues/spawn')
 
-const upgrade = (creep) => {
+const upgradeJob = (creep) => {
   const controller = creep.room.controller
 
   if (creep.upgradeController(controller) === ERR_NOT_IN_RANGE) {
@@ -8,30 +8,44 @@ const upgrade = (creep) => {
   }
 }
 
-module.exports = {
-  spawn: function (numberOfUpgraders) {
-    SpawnQueue.add({
-      key: `upgrader-${numberOfUpgraders}`,
-      name: `upgrader-${numberOfUpgraders}`,
-      body: [MOVE, WORK, CARRY],
-      memory: { role: 'upgrader' },
-      priority: 2
-    })
-  },
-  tick: function (creep) {
-    if (creep.carry.energy < creep.carryCapacity) {
-      const spawn = Game.spawns.Spawn1
-      const withdraw = creep.withdraw(spawn, RESOURCE_ENERGY)
+const withdrawJob = (creep) => {
+  const spawn = Game.spawns.Spawn1
+  const withdraw = creep.withdraw(spawn, RESOURCE_ENERGY)
 
-      if (withdraw === ERR_NOT_IN_RANGE) {
-        if (creep.carry.energy) {
-          upgrade(creep)
-        } else {
-          creep.moveTo(spawn)
-        }
-      }
-    } else {
-      upgrade(creep)
-    }
+  if (withdraw === ERR_NOT_IN_RANGE) {
+    creep.moveTo(spawn)
   }
+}
+
+const tick = (creep) => {
+  if (!creep.carry.energy) {
+    creep.memory.task = 'withdraw'
+  }
+  if (creep.carry.energy < creep.carryCapacity) {
+    if (creep.memory.task === 'withdraw') {
+      withdrawJob(creep)
+    }
+
+    if (creep.memory.task === 'upgrade') {
+      upgradeJob(creep)
+    }
+  } else {
+    creep.memory.task = 'upgrade'
+    upgradeJob(creep)
+  }
+}
+
+const spawn = (numberOfUpgraders) => {
+  SpawnQueue.add({
+    key: `upgrader-${numberOfUpgraders}`,
+    name: `upgrader-${numberOfUpgraders}`,
+    body: [MOVE, WORK, CARRY],
+    memory: { role: 'upgrader' },
+    priority: 2
+  })
+}
+
+module.exports = {
+  spawn,
+  tick
 }
