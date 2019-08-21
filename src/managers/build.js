f7f67ca3f10dd4776b974f590b9812e1c2ab72cb
Paweł <ppierzc@gmail.com>
2019-08-21 17:52:53 +0100

    [FEATURE] build container

--- src/managers/build.js
+++ src/managers/build.js
@@ -9,7 +9,7 @@ module.exports = () => {
   const containersUnderConstruction = Object.values(constructionSites).filter(constructionSite => constructionSite.structureType === STRUCTURE_CONTAINER)
   const containers = Object.values(structures).filter(structure => structure.structureType === STRUCTURE_CONTAINER)
 
-  if (!containersUnderConstruction && !containers) {
+  if (!containersUnderConstruction.length && !containers.length) {
     const res = room.createConstructionSite(25, 30, STRUCTURE_CONTAINER)
     console.log(res)
   }
