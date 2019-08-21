ae0b428d1ce3abd92edddb39ca09d60c0daa2b1c
Paweł <ppierzc@gmail.com>
2019-08-21 17:54:46 +0100

    [FEATURE] build container position

--- src/managers/build.js
+++ src/managers/build.js
@@ -2,15 +2,16 @@ module.exports = () => {
   const {
     rooms,
     constructionSites,
-    structures
+    structures,
+    spawns
   } = Game
   const room = Object.values(rooms)[0]
+  const spawn = Object.values(spawns)[0]
 
   const containersUnderConstruction = Object.values(constructionSites).filter(constructionSite => constructionSite.structureType === STRUCTURE_CONTAINER)
   const containers = Object.values(structures).filter(structure => structure.structureType === STRUCTURE_CONTAINER)
 
   if (!containersUnderConstruction.length && !containers.length) {
-    const res = room.createConstructionSite(25, 30, STRUCTURE_CONTAINER)
-    console.log(res)
+    room.createConstructionSite(spawn.pos.x, spawn.pos.y + 2, STRUCTURE_CONTAINER)
   }
 }
